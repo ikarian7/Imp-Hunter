944def808b9305ed3df0e655c3fb23b277335776
Reggie Schildmeijer <reggie.schildmeijer@gmail.com>
2019-04-14 10:54:35 +0200

    Adds the GameManagement framework and sets the screen size.

---
+++ ImpHunter/ImpHunter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ImpHunter {
+  
+    public class ImpHunter : GameEnvironment {
+        public ImpHunter() {
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent() {
+            base.LoadContent();
+            Screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+
+        }
+
+    }
+}
